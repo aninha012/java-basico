33c4f6bb7bc33497bab98686cedbd630c0f8fc30
ana <anarluisar12@gmail.com>
2024-07-25 16:17:07 -0300

    teste

--- meu-primeiro-pojeto-java/src/App.java
+++ meu-primeiro-pojeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Vamos começar!");
+        System.out.println("Bem Vindos!");
     }
 }
